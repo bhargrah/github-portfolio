f0ade74d7682e4e082642379b1f3f90c1de9643a
Rahul Bhargava <rahulbhargava21@gmail.com>
2024-05-09 18:40:38 +0530

    Initial commit

---
+++ data/user-data.js
@@ -0,0 +1,14 @@
+export const userData = {
+  githubUser: 'bhargrah',
+  devUsername: "bhargrah",
+  github: 'https://github.com/bhargrah',
+  linkedIn: 'https://www.linkedin.com/in/rahulkumarbhargava/',
+  twitter: 'https://twitter.com/said7388',
+  stackOverflow: 'https://stackoverflow.com/users/12253441/rahul-kumar-bhargava',
+  leetcode: "https://leetcode.com/bhargrah/",
+  resume: "https://drive.google.com/file/d/1ekAMXaLkjAXx9a4_icanbxZu_1M8hoT2/view?usp=drive_link",
+  skills: ['Java 8/11/17', 'Scala', 'Python','Rust', 'Shell'],
+  frameworks: ['Spring Boot','Spring Cloud'],
+  technologies: ['Kafka', 'Redis', 'Elastic Search', 'MongoDB', 'Alfresco', 'Grafana', 'Kanban', 'GIT', 'AWS', 'Docker', 'Treadmill', 'Jenkins', 'Maven'],
+  timezone: '+5:30'
+};
